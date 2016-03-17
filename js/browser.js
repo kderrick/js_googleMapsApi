3fa7013633b5100b888a0f7b9e3be3368167e320
Kyle Derrick and Sara Wagner <sara.wagner23q@gmail.com>
2016-03-17 09:11:28 -0700

    functioning basic example map using cheat sheet

--- js/browser.js
+++ js/browser.js
@@ -1,1 +1,12 @@
-//Business Logic
+// //Business Logic
+// var map;
+//
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: -34.397, lng: 150.644},
+//     zoom: 8
+//   });
+// }
+//
+//
+// exports.initMap = initMap;
